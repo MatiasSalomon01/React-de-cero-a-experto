7de34c8269f25bca7f11b79b66bc7dba70e90966
=Matias Salomon <matias.salomon@clt.com.py>
2023-07-10 15:31:40 -0400

    Nuestro primer Componente

--- Seccion 4 - Primeros pasos en React/counter-app/src/main.jsx
+++ Seccion 4 - Primeros pasos en React/counter-app/src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
+import { App } from "./HelloWorldApp";
 
-function App(){
-    return <h1>Hola Mundo!!!</h1>;
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
