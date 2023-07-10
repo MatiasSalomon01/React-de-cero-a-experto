8af5947b7f6b76c10434348a67306dc846839dc5
=Matias Salomon <matias.salomon@clt.com.py>
2023-07-10 15:17:05 -0400

    Hola Mundo en React

--- Seccion 4 - Primeros pasos en React/counter-app/src/main.jsx
+++ Seccion 4 - Primeros pasos en React/counter-app/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+function App(){
+    return <h1>Hola Mundo!!!</h1>;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
