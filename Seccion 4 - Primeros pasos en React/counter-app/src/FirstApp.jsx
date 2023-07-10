e17fdf98a5a71912ec9343a1343b55c65d3a6ee0
=Matias Salomon <matias.salomon@clt.com.py>
2023-07-10 15:40:29 -0400

    Retornar elementos en el Componente - Fragment

--- Seccion 4 - Primeros pasos en React/counter-app/src/FirstApp.jsx
+++ Seccion 4 - Primeros pasos en React/counter-app/src/FirstApp.jsx
@@ -2,5 +2,10 @@
 
 
 export const FirstApp = () => {
-    return <h2>First App</h2>
+    return (
+        <>
+            <h2>First App</h2>
+            <p>Soy un subtitulo</p>
+        </>
+    )
 }
