e21f971364850ad6be531f6d57f5166308ce530e
=Matias Salomon <matias.salomon@clt.com.py>
2023-07-11 12:28:31 -0400

    handleSubtract y handleReset

--- Seccion 4 - Primeros pasos en React/counter-app/src/Tarea.jsx
+++ Seccion 4 - Primeros pasos en React/counter-app/src/Tarea.jsx
@@ -4,17 +4,21 @@ import { useState } from 'react';
 
 export const Tarea = ({ value }) => {
 
-    const [ counter, setCounter ] = useState( value );
+    const [counter, setCounter] = useState(value);
 
-    const handleAdd = () => {
-        setCounter(counter+1);
-    }
+    const handleAdd = () => setCounter(counter + 1);
+
+    const handleMinus = () => setCounter(counter - 1);
+
+    const handleReset = () => setCounter(value);
 
     return (
-        <> 
+        <>
             <h1>Tarea</h1>
-            <h2>{ counter }</h2>
-            <button onClick={ handleAdd }>+1</button>
+            <h2>{counter}</h2>
+            <button onClick={handleAdd}>+1</button>
+            <button onClick={handleMinus}>-1</button>
+            <button onClick={handleReset}>Reset</button>
         </>
     )
 }
