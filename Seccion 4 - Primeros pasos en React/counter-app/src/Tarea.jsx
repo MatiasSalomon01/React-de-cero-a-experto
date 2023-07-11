645f28749ccda73f07ba0291d604a8e811296106
=Matias Salomon <matias.salomon@clt.com.py>
2023-07-11 12:10:28 -0400

    useState - Hook

--- Seccion 4 - Primeros pasos en React/counter-app/src/Tarea.jsx
+++ Seccion 4 - Primeros pasos en React/counter-app/src/Tarea.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-export const Tarea = ({value}) => {
+export const Tarea = ({ value }) => {
+
+    const [ counter, setCounter ] = useState( value );
 
     const handleAdd = () => {
-        console.log('+1')
+        setCounter(counter+1);
     }
 
     return (
-        <>
+        <> 
             <h1>Tarea</h1>
-            <h2>{ value }</h2>
+            <h2>{ counter }</h2>
             <button onClick={ handleAdd }>+1</button>
         </>
     )
