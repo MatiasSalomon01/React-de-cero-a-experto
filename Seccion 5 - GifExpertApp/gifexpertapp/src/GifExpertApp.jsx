88b89a40f85f24e7648f905c96468847c9a9ac9b
=Matias Salomon <matias.salomon@clt.com.py>
2023-07-11 14:58:24 -0400

    Agregar una nueva categoría

--- Seccion 5 - GifExpertApp/gifexpertapp/src/GifExpertApp.jsx
+++ Seccion 5 - GifExpertApp/gifexpertapp/src/GifExpertApp.jsx
@@ -3,6 +3,11 @@ import { useState } from "react"
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['One Punch', 'Drangon Ball'])
+
+    const onAddCategory = () => {
+        setCategories([ ...categories, 'Valorant' ]);
+    }
+
     return (
         <>
             { /* Titulo */ }
@@ -12,6 +17,7 @@ export const GifExpertApp = () => {
             { /* Input */ }
 
             { /* Lista de Gif's */ }
+            <button onClick={ onAddCategory } >Agregar</button>
             <ol>
                 { 
                     categories.map( category => {
