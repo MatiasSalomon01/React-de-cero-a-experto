9eab11173f68b38c691aa42af8d79d9326c377b5
=Matias Salomon <matias.salomon@clt.com.py>
2023-07-12 16:34:52 -0400

    useState

--- Seccion 9 - Profundizando Hooks - Generales/src/main.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import HookApp from './HookApp'
 import "bootstrap/dist/css/bootstrap.min.css"
+import CounterApp from './01-useState/CounterApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HookApp/>
+    <CounterApp/>
   </React.StrictMode>,
 )
