a05086e0813a187d696cdbdce271cac55828f32b
=Matias Salomon <matias.salomon@clt.com.py>
2023-07-12 11:02:55 -0400

    className - Clases de css

--- Seccion 5 - GifExpertApp/gifexpertapp/src/components/GifGrid.jsx
+++ Seccion 5 - GifExpertApp/gifexpertapp/src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
+import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
 
@@ -18,11 +19,17 @@ export const GifGrid = ({ category }) => {
     return (
     <>
         <h3>{ category }</h3>
-        <ol>
+        <div className="card-grid">
             {
-                images.map( ({ id, title }) => <li  key={ id }>{ title }</li> )
+                images.map( 
+                    ( image ) => 
+                    <GifGridItem
+                        key={ image.id }
+                        { ...image }
+                    />
+                )
             }
-        </ol>
+        </div>
     </>
   )
 }
