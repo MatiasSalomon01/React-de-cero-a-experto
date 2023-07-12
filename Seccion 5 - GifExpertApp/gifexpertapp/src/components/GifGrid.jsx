fce613b470adbe784dd68e56072f89a2513f733d
=Matias Salomon <matias.salomon@clt.com.py>
2023-07-12 10:25:50 -0400

    Demostrcion de produccion rapido

--- Seccion 5 - GifExpertApp/gifexpertapp/src/components/GifGrid.jsx
+++ Seccion 5 - GifExpertApp/gifexpertapp/src/components/GifGrid.jsx
@@ -3,8 +3,6 @@ import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
-    const [counter, setCounter] = useState(10)
-
     useEffect( () => {
         getGifs(category);
     }, [] )
@@ -13,10 +11,6 @@ export const GifGrid = ({ category }) => {
     return (
     <>
         <h3>{ category }</h3>
-        <h5>{ counter }</h5>
-        <button onClick={ () => setCounter(counter+1) }>
-            +1
-        </button>
     </>
   )
 }
--- Seccion 5 - GifExpertApp/gifexpertapp/src/helpers/getGifs.js
+++ Seccion 5 - GifExpertApp/gifexpertapp/src/helpers/getGifs.js
@@ -11,6 +11,5 @@ export const getGifs = async (category) => {
         url: img.images.downsized_medium.url
     }));
 
-    console.log(gifs)
     return gifs;
 } 
