bb4e4cf646b12f8b44bf549ffd95f3011ee43fdd
=Matias Salomon <matias.salomon@clt.com.py>
2023-07-13 14:33:57 -0400

    useRef - Primer uso

--- Seccion 9 - Profundizando Hooks - Generales/src/main.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/src/main.jsx
@@ -5,11 +5,12 @@ import "bootstrap/dist/css/bootstrap.min.css"
 // import CounterWithCustomHook from './01-useState/CounterWithCustomHook'
 // import SimpleForm from './02-useEffect/SimpleForm'
 // import FormWithCustomHook from './02-useEffect/FormWithCustomHook'
-import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
+// import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
+import FocusScreen from './04-useRef/FocusScreen'
 // import CounterApp from './01-useState/CounterApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <MultipleCustomHooks/>
+    <FocusScreen/>
   // </React.StrictMode>,
 )
