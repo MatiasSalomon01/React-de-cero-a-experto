1019590dc2a9d209518125e70b8af101e37f63f1
=Matias Salomon <matias.salomon@clt.com.py>
2023-07-14 09:55:21 -0400

    Continuación del Proyecto - HookApp

---
+++ Seccion 10 - Profundizando Hooks - useReducer/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './intro-reducer'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+    <h1>asd</h1>
+  // </React.StrictMode>,
+)
