0fd0a2aa120bf5c052d9e2e06bfb9afb531b682f
=Matias Salomon <matias.salomon@clt.com.py>
2023-07-17 10:42:35 -0400

    Creando el cascarón de la lista de TODOs

--- Seccion 10 - Profundizando Hooks - useReducer/src/TodoApp.jsx
+++ Seccion 10 - Profundizando Hooks - useReducer/src/TodoApp.jsx
@@ -6,7 +6,7 @@ const initialState = [
         id: new Date().getTime(),
         description: 'Recolectar la piedra del Alma',
         done: false
-    }, 
+    },
     {
         id: new Date().getTime() * 3,
         description: 'Recolectar la piedra del Alma',
@@ -16,19 +16,42 @@ const initialState = [
 
 const TodoApp = () => {
 
-    const [state, dispatch] = useReducer(todoReducer, initialState)
+    const [todos, dispatch] = useReducer(todoReducer, initialState)
 
 
     return (
         <>
-            <h1>TodoApp</h1>
+            <h1>
+                TodoApp: 10,<small>pendientes: 2</small>
+            </h1>
             <hr />
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group">
+                        {
+                            todos.map((todo) => (
+                                <li key={todo.id} className="list-group-item d-flex justify-content-between">
+                                    <span className="align-self-center">Item 1</span>
+                                    <button className="btn btn-danger">Borrar</button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Agregar TODO</h4>
+                    <hr />
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Que hay que hacer?"
+                            className="form-control"
+                        />
+                        <button type="submit" className="btn btn-outline-primary mt-1">Agregar</button>
+                    </form>
+                </div>
+            </div>
 
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-            </ul>
         </>
     )
 }
